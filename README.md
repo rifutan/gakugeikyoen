b7a78d84930f694379cadefa45ddb5826e043b27
Shota Ishizaki <rumrum.merry@gmail.com>
2017-03-16 19:17:19 +0900

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+Gakugei-kyoen
+====
+
+## 事前に用意するもの
+- node.js
+
+## 環境構築
+```
+npm install
+npm start
+```
---
+++ src/js/script.js
@@ -0,0 +1,43 @@
+import $ from 'jquery';
+var slick = require('./lib/slick.js');
+
+var loader_desc = ["－ 火曜日の夜、学術と芸術を求めんとする若者が駒場に集う －", "－ 駒場で紡がれるアートとアカデミアの共創を目撃せよ －", "－東大生の創造性を限界まで －", "－世界の深みを知る術、学と芸の融合の先で －"]
+var loader_desc_num = Math.floor(Math.random() * 4);
+$('.loader__desc').html(loader_desc[loader_desc_num]);
+
+$(function() {
+  var h = $(window).height();
+  $('.wrapper').css('display','none');
+  $('.loader ,.loader__inner').height(h).css('display','block');
+});
+
+$(window).load(function () {
+  $('.loader').delay(1000).fadeOut(800);
+  $('.loader__inner').delay(1000).fadeOut(300);
+  $('.wrapper').css('display', 'block');
+});
+
+function stopload(){
+}
+$(function(){
+  setTimeout(function(){
+    $('.wrapper').css('display','block');
+    $('.loader').delay(300).fadeOut(800);
+    $('.loader__inner').delay(300).fadeOut(300);
+  },4000);
+});
+
+$(function() {
+    $('.js-slider').slick({
+      infinite: true,
+      arrows: false,
+      dots: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      centerMode: true,
+      autoplay: true,
+      autoplaySpeed: 5000,
+      speed: 800,
+      pauseOnHover: false,
+    });
+});
